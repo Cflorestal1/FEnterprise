10ed418997ad2c8b2c404d965f5228df4dfc8edc
Charlie Brown <c.florestal.com>
2018-07-31 00:07:03 -0400

    basic website structure setup

--- web/index.php
+++ web/index.php
@@ -1,5 +1,8 @@
 <?php 
-	include 'loader.php';
-
-
-	echo $twig->render('index.html',[]);
+	include './loader.php';
+	
+	$home = [
+		'title' => 'Florestal Enterprise'
+	];
+	
+	echo $twig->render('index.html',['home' => $home ]);
--- web/loader.php
+++ web/loader.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once '../vendor/autoloader.php';
+require_once '../vendor/autoload.php';
 
-$loader = new Twig_Loader_Filesystem(__DIR_.'/../templates' );
+$loader = new Twig_Loader_Filesystem('../templates' );
 
 $twig = new Twig_Environment($loader);
 
